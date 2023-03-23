1d820c06b0bbe4c4373b6f707bca212fb36f6bb2
jake-fawcett <jakefawcett2298@hotmail.co.uk>
2023-03-23 20:29:02 +0000

    initial hello world webapp

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(hello_handler))
+        .route("/health", get(health_handler));
+
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn hello_handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
+
+async fn health_handler() -> &'static str {
+    "Healthy"
+}
