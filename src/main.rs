0789b915f748e38af073464764653c4ac86d644c
jake-fawcett <jakefawcett2298@hotmail.co.uk>
2023-03-28 20:29:21 +0100

    added html templates for main, health endpoint and hello endpoint

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ async fn main() {
     // build our application with a route
     let app = Router::new()
         .route("/", get(hello_handler))
-        .route("/health", get(health_handler));
+        .route("/health", get(health_handler))
+        .route("/hello/:name", get(hello));
 
     // run it
     let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
@@ -17,12 +18,55 @@ async fn main() {
         .unwrap();
 }
 
-async fn hello_handler() -> Html<&'static str> {
+async fn main_handler() -> impl IntoResponse {
     println!("/ called");
-    Html("<h1>Hello, World!</h1>")
+    let template = MainTemplate;
+    HtmlTemplate(template)
 }
 
-async fn health_handler() -> &'static str {
+async fn health_handler() -> impl IntoResponse {
     println!("/health called");
-    "Healthy"
+    let template = HealthTemplate;
+    HtmlTemplate(template)
+}
+
+async fn hello_handler(extract::Path(name): extract::Path<String>) -> impl IntoResponse {
+    println_f!("/hello/{name} called");
+    let template = HelloTemplate { name };
+    HtmlTemplate(template)
+}
+
+
+#[derive(Template)]
+#[template(path = "main.html")]
+struct MainTemplate {
+}
+
+#[derive(Template)]
+#[template(path = "health.html")]
+struct HealthTemplate {
+}
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate {
+    name: String,
+}
+
+struct HtmlTemplate<T>(T);
+
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
 }
